c2decc51e956aa189d0fd31d7cb2babb5ce28a52
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 15:47:15 +0200

    add hot reload

--- Web/devServer.js
+++ Web/devServer.js
@@ -2,14 +2,13 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
-var port = 80;
 
 var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
@@ -22,11 +21,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(1235, function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-    console.log('Listening at port' + port);
+  console.log('Listening at http://localhost:1235');
 });
