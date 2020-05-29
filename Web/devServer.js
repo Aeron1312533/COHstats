1781bfa5d935b94558f5df89db85fcbf23c85812
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 14:29:37 +0200

    port forwarding to frontend/backend

--- Web/devServer.js
+++ Web/devServer.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
-var port = 80;
+var port = 1312;
 
 var app = express();
 var compiler = webpack(config);
--- Web/proxy.js
+++ Web/proxy.js
@@ -1,8 +1,8 @@
 var proxy = require('redbird')({port: 80});
 
-proxy.register("agilegateway.com", "http://80.211.202.8:1235");
-proxy.register("agilegateway.com/api", "http://80.211.202.8:1234");
-proxy.register("www.agilegateway.com", "http://80.211.202.8:1235");
-proxy.register("http://80.211.202.8/api", "http://80.211.202.8:1234");
-proxy.register("http://80.211.202.8", "http://80.211.202.8:1235");
-proxy.register("https://agilegateway.com", "http://80.211.202.8:1235");
+proxy.register("agilegateway.com", "http://34.65.141.148:1312");
+proxy.register("agilegateway.com/api", "http://34.65.141.148:1234");
+proxy.register("www.agilegateway.com", "http://34.65.141.148:1312");
+proxy.register("http://34.65.141.148/api", "http://34.65.141.148:1234");
+proxy.register("http://834.65.141.148", "http://34.65.141.148:1312");
+proxy.register("https://agilegateway.com", "http://34.65.141.148:1312");
