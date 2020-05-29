f1c695ef4061c98f7cb08d248dc61758505c937b
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 15:17:24 +0200

    fix error

--- Web/runServer.js
+++ Web/runServer.js
@@ -12,7 +12,6 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('/sitemap.xml', function(req, res) {
   res.sendFile(path.join(__dirname, 'sitemap.xml'));
