e45899f43518b2d2c720adf65a2c8dfb7a350f88
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 15:21:52 +0200

    fix

--- Web/runServer.js
+++ Web/runServer.js
@@ -7,12 +7,6 @@ var port = 80;
 var app = express();
 var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-
 app.get('/sitemap.xml', function(req, res) {
   res.sendFile(path.join(__dirname, 'sitemap.xml'));
 });
