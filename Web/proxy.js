539f54cb0b0a58b43fc85b2cdb84fc28b27d553e
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 14:42:56 +0200

    remove reverseproxy

--- Web/proxy.js
+++
@@ -1,8 +0,0 @@
-var proxy = require('redbird')({port: 80});
-
-proxy.register("agilegateway.com", "http://34.65.141.148:1312");
-proxy.register("agilegateway.com/api", "http://34.65.141.148:1234");
-proxy.register("www.agilegateway.com", "http://34.65.141.148:1312");
-proxy.register("http://34.65.141.148/api", "http://34.65.141.148:1234");
-proxy.register("http://34.65.141.148", "http://34.65.141.148:1312");
-proxy.register("https://agilegateway.com", "http://34.65.141.148:1312");
