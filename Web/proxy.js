869372dda2124a7d6c84696d4dc56f0c6710d5a6
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-29 14:36:31 +0200

    fixed port in reverse proxy

--- Web/proxy.js
+++ Web/proxy.js
@@ -4,5 +4,5 @@ proxy.register("agilegateway.com", "http://34.65.141.148:1312");
 proxy.register("agilegateway.com/api", "http://34.65.141.148:1234");
 proxy.register("www.agilegateway.com", "http://34.65.141.148:1312");
 proxy.register("http://34.65.141.148/api", "http://34.65.141.148:1234");
-proxy.register("http://834.65.141.148", "http://34.65.141.148:1312");
+proxy.register("http://34.65.141.148", "http://34.65.141.148:1312");
 proxy.register("https://agilegateway.com", "http://34.65.141.148:1312");
