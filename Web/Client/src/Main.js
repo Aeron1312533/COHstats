0afea59373273c7708f46afde7b7e4f2a397e675
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-31 22:22:59 +0200

    fix items

--- Web/Client/src/Main.js
+++ Web/Client/src/Main.js
@@ -33,6 +33,7 @@ export default class Main extends React.Component {
     }
 
     render() {
+        const { error, isLoaded, items } = this.state;
         console.log(items);
         return (
             <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/231430/c89f060dc361fa5fb8b9ad1368c91651a1204ce2.jpg" alt="logo" />
