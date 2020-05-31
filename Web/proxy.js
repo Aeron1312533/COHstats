d5f366381d0814012d1f964ad071a7f172694e7f
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-31 22:00:32 +0200

    localhost

--- Web/proxy.js
+++ Web/proxy.js
@@ -1,10 +1,10 @@
 var proxy = require('redbird')({port: 80});
 
-proxy.register("coh2stats.online", "http://81.2.251.32:82");
+proxy.register("coh2stats.online", "localhost:82");
 proxy.register("www.coh2stats.online", "http://81.2.251.32:82");
 proxy.register("http://coh2stats.online", "http://81.2.251.32:82");
 proxy.register("http://81.2.251.32", "http://81.2.251.32:82");
-proxy.register("coh2stats.online/api", "http://81.2.251.32:81");
+proxy.register("coh2stats.online/api", "localhost:81");
 proxy.register("www.coh2stats.online/api", "http://81.2.251.32:81");
 proxy.register("http://coh2stats.online/api", "http://81.2.251.32:81");
 proxy.register("http://81.2.251.32/api", "http://81.2.251.32:81");
