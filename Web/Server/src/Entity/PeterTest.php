3169d52e450a1c7f8fba83ef253bc952352476f9
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-31 19:03:27 +0200

    repository class

--- Web/Server/src/Entity/PeterTest.php
+++ Web/Server/src/Entity/PeterTest.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  * @ORM\Table(name="PeterTest")
  */
 class PeterTest
