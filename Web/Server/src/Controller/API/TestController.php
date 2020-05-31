f83ec451c9e65ce7a111b62de77ad5a75de9fa7f
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-31 19:06:31 +0200

    validator

--- Web/Server/src/Controller/API/TestController.php
+++ Web/Server/src/Controller/API/TestController.php
@@ -9,11 +9,12 @@ use App\Entity\PeterTest;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
  
 class TestController extends AbstractController
 {
 
-    public function test(): Response
+    public function test(ValidatorInterface $validator): Response
     {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
@@ -26,7 +27,7 @@ class TestController extends AbstractController
 
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
-
+        $errors = $validator->validate($product);
         return new Response('Saved new product with id '.$product->getId());
     }
 }
