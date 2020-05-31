7dd2a282409d8f7058ff4539e38d1e520734e80f
Aeron1312533 <pekarovic.peter@gmail.com>
2020-05-31 18:18:45 +0200

    test integration DB

--- Web/Server/src/Controller/API/TestController.php
+++ Web/Server/src/Controller/API/TestController.php
@@ -1,16 +1,32 @@
 <?php
+
+
+// src/Controller/ProductController.php
 namespace App\Controller\API;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+// ...
+use App\Entity\PeterTest;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
-class TestController
+class TestController extends AbstractController
 {
-    public function test()
-    {
-        $number = random_int(0, 100);
 
-        return new JsonResponse(
-            ['response' => 'This is test response from API']
-        );
+    public function test(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new PeterTest();
+        $product->setValue('Keyboard');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
     }
 }
---
+++ Web/Server/src/Entity/PeterTest.php
@@ -0,0 +1,31 @@
+// src/Entity/Product.php
+namespace App\Entity;
+
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PeterTestRepository")
+ */
+class PeterTest
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $value;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // ... getter and setter methods
+}
