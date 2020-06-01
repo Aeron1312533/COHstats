1b3addbf3bc05ea212c11db14380d171a0673ccc
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-01 19:23:42 +0200

    debug

--- Web/Client/src/Component/FractionsList.js
+++ Web/Client/src/Component/FractionsList.js
@@ -14,8 +14,10 @@ export default class FractionsList extends React.Component {
     list = () => {
         return this.state.fractions.map(element => {
             let isActive = (element == this.state.active) ? true : false;
+            console.log(element);
+            console.log(isActive);
             return (
-                <FractionListItem type={element} text="random" active={isActive}/>
+                <FractionListItem type={element} text="" active={isActive} />
             );
         });
     };
