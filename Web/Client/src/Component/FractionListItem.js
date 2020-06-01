09dbd5a8117e765a665cbbb9c5aae7308515d922
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-01 19:03:19 +0200

    link to fractions

--- Web/Client/src/Component/FractionListItem.js
+++ Web/Client/src/Component/FractionListItem.js
@@ -12,9 +12,10 @@ export default class FractionListItem extends React.Component {
 
     render() {
         let source = "/Images/Fractions/" + this.state.type + ".png";
+        let href = "/" + + this.state.type;
         return (
             <li className = "fraction">
-                <img src={source} alt={this.state.text} />
+                <a href={href} ><img src={source} alt={this.state.text} /></a>
             </li>
         )
     }
