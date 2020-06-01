fece20a28164ab3cccb589d7bc708b0cd08d23c8
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-01 19:08:35 +0200

    location

--- Web/Client/src/Component/FractionListItem.js
+++ Web/Client/src/Component/FractionListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Styles/FractionListItem.css';
+import { useLocation } from 'react-router-dom';
 
 export default class FractionListItem extends React.Component {
     constructor(props) {
@@ -14,6 +15,8 @@ export default class FractionListItem extends React.Component {
         let source = "/Images/Fractions/" + this.state.type + ".png";
         let href = "/" + this.state.type;
         let imgClass = "";
+        let location = useLocation();
+        console.log(location);
         return (
             <li className = "fraction">
                 <a href={href} ><img src={source} alt={this.state.text} /></a>
