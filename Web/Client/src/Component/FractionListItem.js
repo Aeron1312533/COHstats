b46070c954a5b3adbf3ffb037e47cc638b7a260b
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-01 19:18:20 +0200

    fix invalid hook

--- Web/Client/src/Component/FractionListItem.js
+++ Web/Client/src/Component/FractionListItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../Styles/FractionListItem.css';
-import { useLocation } from 'react-router-dom';
 
 export default class FractionListItem extends React.Component {
     constructor(props) {
@@ -16,8 +15,7 @@ export default class FractionListItem extends React.Component {
         let source = "/Images/Fractions/" + this.state.type + ".png";
         let href = "/" + this.state.type;
         let imgClass = (this.state.isActive == true) ? "active" : "";
-        let location = useLocation();
-        console.log(location);
+
         return (
             <li className = "fraction">
                 <a href={href} ><img className={imgClass} src={source} alt={this.state.text} /></a>
