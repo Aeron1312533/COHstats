2bd78c001a308a083c1dc2172ad6a35942305ae5
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-02 19:03:40 +0200

    ability

---
+++ Web/Client/src/Component/CommanderAbility.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Styles/CommanderAbility.css';
+
+export default class CommanderAbility extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: props.type
+        };
+    }
+
+    render() {
+        let source = "/Images/Commanders/Abilities/" + this.state.type + ".png";
+        //let href = "/" + this.state.type;
+        //let imgClass = (this.state.isActive == true) ? "active" : "";
+
+        return (
+            <img className="commanderAbility" src={source} alt="text" />
+        )
+    }
+}
--- Web/Client/src/Component/CommanderListItem.js
+++ Web/Client/src/Component/CommanderListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Styles/CommanderListItem.css';
+import CommanderAbility from './CommanderAbility';
 
 export default class CommanderListItem extends React.Component {
     constructor(props) {
@@ -13,18 +14,17 @@ export default class CommanderListItem extends React.Component {
 
     render() {
         let source = "/Images/Commanders/" + this.state.type + ".png";
-        let sourceAbility = "/Images/Commanders/Abilities/1.png";
         //let href = "/" + this.state.type;
         //let imgClass = (this.state.isActive == true) ? "active" : "";
 
         return (
             <li className="commanderListItem">
                 <img className="commanderIcon" src={source} alt={this.state.text} />
-                <img className="commanderAbility" src={sourceAbility} alt={this.state.text} />
-                <img className="commanderAbility" src={sourceAbility} alt={this.state.text} />
-                <img className="commanderAbility" src={sourceAbility} alt={this.state.text} />
-                <img className="commanderAbility" src={sourceAbility} alt={this.state.text} />
-                <img className="commanderAbility" src={sourceAbility} alt={this.state.text} />
+                <CommanderAbility type="1" />
+                <CommanderAbility type="2" />
+                <CommanderAbility type="3" />
+                <CommanderAbility type="4" />
+                <CommanderAbility type="5" />
             </li>
         )
     }
