a498a28d38b0abe6301b529270c8371527895b27
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-03 19:09:54 +0200

    test

--- Web/Client/src/Component/Fefe.js
+++ Web/Client/src/Component/Fefe.js
@@ -7,11 +7,12 @@ export default class Fefe extends React.Component {
     }
 
     render() {
-        console.log("fefe");
+        console.log("fefe")
         return (
             <div>
             <   h1>Fefe rulezz!</h1>
                 <img src="/Images/Fractions/british.png" alt="logo" />
+                <Menu />
             </div>
         )
     }
