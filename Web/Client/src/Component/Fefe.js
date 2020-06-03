f250c4ecc220adc95443cd2546b01b20c2135f01
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-03 19:26:29 +0200

    test

--- Web/Client/src/Component/Fefe.js
+++ Web/Client/src/Component/Fefe.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Menu from './Menu';
 
 export default class Fefe extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         console.log("fefe")
