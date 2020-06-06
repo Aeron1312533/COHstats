4ede8fbbca365e816a1fb36a6ff23fbd8b8e61b4
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-06 08:12:41 +0200

    css

--- Web/Client/src/Component/CommandersList.js
+++ Web/Client/src/Component/CommandersList.js
@@ -58,23 +58,19 @@ export default class CommandersList extends React.Component {
             superLargeDesktop: {
                 // the naming can be any, depends on you.
                 breakpoint: { max: 4000, min: 3000 },
-                items: 3,
-                partialVisibilityGutter: 40
+                items: 3
             },
             desktop: {
                 breakpoint: { max: 3000, min: 1100 },
-                items: 3,
-                partialVisibilityGutter: 40
+                items: 3
             },
             tablet: {
                 breakpoint: { max: 1100, min: 464 },
-                items: 2,
-                partialVisibilityGutter: 30
+                items: 2
             },
             mobile: {
                 breakpoint: { max: 464, min: 0 },
-                items: 1,
-                partialVisibilityGutter: 20
+                items: 1
             }
         };
         return (
@@ -85,7 +81,6 @@ export default class CommandersList extends React.Component {
                     slidesToSlide={2}
                     showDots
                     renderDotsOutside
-                    partialVisible
                 >
                     {this.list()}
                 </Carousel>
