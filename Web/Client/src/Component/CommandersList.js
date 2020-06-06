a2bffa703d8d85d327cd00555eb58509af8a4d2d
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-06 08:08:37 +0200

    carousel responsivity

--- Web/Client/src/Component/CommandersList.js
+++ Web/Client/src/Component/CommandersList.js
@@ -58,14 +58,14 @@ export default class CommandersList extends React.Component {
             superLargeDesktop: {
                 // the naming can be any, depends on you.
                 breakpoint: { max: 4000, min: 3000 },
-                items: 5
+                items: 3
             },
             desktop: {
-                breakpoint: { max: 3000, min: 1024 },
+                breakpoint: { max: 3000, min: 1100 },
                 items: 3
             },
             tablet: {
-                breakpoint: { max: 1024, min: 464 },
+                breakpoint: { max: 1100, min: 464 },
                 items: 2
             },
             mobile: {
