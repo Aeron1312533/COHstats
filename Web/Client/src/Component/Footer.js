1ebe4f59fd46b06658551d3826c7f4397bab6092
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-15 16:52:21 +0200

    copyright

--- Web/Client/src/Component/Footer.js
+++ Web/Client/src/Component/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
-                toto je footer
+                Copyright © 2020 coh2stats.online
             </div>
         )
     }
