82294efd1fec059f3f0b8e0f853fd95bf7edc3e4
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-15 17:11:55 +0200

    css

--- Web/Client/src/Component/ContactForm.js
+++ Web/Client/src/Component/ContactForm.js
@@ -12,8 +12,8 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="ContactForm">
-                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+            <div className="contactFormWrapper">
+                <form id="contactForm" onSubmit={this.handleSubmit.bind(this)} method="POST">
                     <div className="form-group">
                         <label htmlFor="name">Name</label>
                         <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
@@ -26,7 +26,7 @@ export default class App extends React.Component {
                         <label htmlFor="message">Message</label>
                         <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="contactFormButton">Submit</button>
                 </form>
             </div>
         );
