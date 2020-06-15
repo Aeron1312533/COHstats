e069b8313261fd1e348ea909b073bec506f78d38
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-15 17:07:02 +0200

    contactform

--- Web/Client/src/Component/Footer.js
+++ Web/Client/src/Component/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactForm from './ContactForm';
 import '../Styles/Footer.css';
 
 export default class Footer extends React.Component {
@@ -6,9 +7,14 @@ export default class Footer extends React.Component {
         super(props);
     }
     render() {
+        //feedback/contact us
+        //sitemap
+        //disclaimer
+        //created by
         return (
             <div className="footer">
                 Copyright © 2020 coh2stats.online
+                <ContactForm {...this.props} />
             </div>
         )
     }
