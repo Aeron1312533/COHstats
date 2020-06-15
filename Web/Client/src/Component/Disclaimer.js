5e1c84c8735416e5b735125485de8a862e03706d
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-15 19:11:34 +0200

    disclaimer

--- Web/Client/src/Component/Disclaimer.js
+++ Web/Client/src/Component/Disclaimer.js
@@ -13,7 +13,7 @@ export default class Disclaimer extends React.Component {
         return (
             <div className="disclaimer">
                 <div className="disclaimerLeft">
-                    Copyright &copy; 2020 coh2stats.online
+                    DISCLAIMER: Images and texts used on this fansite are property of Relic Entertainment. We do not own anything. Copyright &copy; 2020 coh2stats.online
                 </div>
                 <div className="disclaimerRight">
                     Created by vladcatvorstva, fefe
