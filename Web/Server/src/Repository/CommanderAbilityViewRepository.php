af3bc9898560fc05713936dc31d3db8008fdddb1
Aeron1312533 <pekarovic.peter@gmail.com>
2020-06-21 16:52:51 +0200

    fix

--- Web/Server/src/Repository/CommanderAbilityViewRepository.php
+++ Web/Server/src/Repository/CommanderAbilityViewRepository.php
@@ -6,5 +6,12 @@ use Doctrine\ORM\EntityRepository;
 
 class CommanderAbilityViewRepository extends EntityRepository
 {
-
+    public function findById($id) {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT cav FROM App\Entity\CommanderAbilityView cav WHERE cav.commanderAbilityKey = :id'
+            )
+            ->setParameter('id', $id)
+            ->getResult();
+    }
 }
